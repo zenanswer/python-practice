aa1dde03835c3c06200773f70eb4abad87d79088
xuechenwang <xuechenwang@cienet.com.cn>
2017-12-07 11:16:32 +0800

    Add wraps

---
+++ src/py/wraps/README.md
@@ -0,0 +1,5 @@
+File list
+====
+
+-   wraps.py - usage of \@wraps and decorator
+-   wrapswithparms.py - give more parms for a decorator
---
+++ src/py/wraps/wraps.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import time
+from functools import wraps
+
+
+def timethis(func):
+    '''
+    Decorator for execution time
+    '''
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(func.__name__, end-start)
+        return result
+
+    # for timethis
+    return wrapper
+
+
+@timethis
+def countdown(n: int):  # countdown.__annotations__
+    '''
+    A count down function
+    '''
+    while n > 0:
+        n -= 1
+
+
+countdown(100000)
+# countdown 0.0066568851470947266
+
+countdown(1000000)
+# countdown 0.0733339786529541
+
+print(countdown.__name__)
+# countdown
+
+print(countdown.__doc__)
+# A count down function
+
+print(countdown.__annotations__)  # def countdown(n:int):
+# {'n': <class 'int'>}
